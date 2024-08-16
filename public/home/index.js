a8e49745cacfab3ac719772c8c461b0e1a007d80
MoizRed <abdoureduniv2000@gmail.com>
2024-08-16 21:42:40 +0100

    client side api requests

--- public/home/index.js
+++
@@ -1,5 +0,0 @@
-
- const getStartedBtn = document.querySelector(".btn");
- getStartedBtn.addEventListener("click", () => {
-     alert("You've clicked the button!");
- });
--- server.mjs
+++ server.mjs
@@ -1,90 +1,67 @@
 import express from "express";
 import dotenv from "dotenv";
 import Log from "./Log.mjs";
+import fs from "fs";
+const DATA = JSON.parse(fs.readFileSync("products.json", "utf8"));
 
+export const products = DATA.products;
 
 dotenv.config();
 const app = express();
-
-
 
 //middlewres
 app.use((req, res, next) => {
   Log(req.method, req.path);
   next();
-})
-app.use(express.json())
-app.use(express.static("public"))  
+});
+
+app.use(express.json());
+app.use(express.static("public"));
 app.use(express.static("public/home"));
-
-
-
-
-const products = [
-  {
-    id: 1,
-    name: "shoes",  
-    price: "1000",
-  },
-  {
-    id: 2,
-    name: "t-shirt", 
-    price: "2000",
-  },
-  {
-    id: 3,
-    name: "jeans",  
-    price: "4000",
-  },
-];
 
 app.listen(process.env.PORT, () => {
   Log(`listening on port http://${process.env.IP}:${process.env.PORT}`);
 });
 
-
 //routes for ALL PRODUCTS
-app.get("/products" , (req,res)=>{
+app.get("/products", (req, res) => {
+  // res.json(products);
 
-  res.json(products)
+  res.sendFile("products.html", { root: "./public/home" });
+});
 
-
-})
-
-
-app.get("/" , (req  , res) =>{
-
-  
-  res.sendFile("index.html", {root : "./public"}) 
-
-})
-
-
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "./public" });
+});
 
 // query param to get Specific Product based on the query search
-app.get("/products/search"  , (req , res)=>{
+app.get("/products/search", (req, res) => {
+  const query = req.query;
+  const name = req.query.name;
+  console.log(name);
 
-  const query = req.query
-  const name  = req.query.name
-  console.log(name)
-
-  const found = products.find( I => I.name == name) //iretates until it returns true
-  !found? res.status(404).send("not found") : res.json(found) 
-  console.log(found)
-})
-
+  const found = products.find((I) => I.name == name); //iretates until it returns true
+  !found ? res.status(404).send("not found") : res.json(found);
+  console.log(found);
+});
 
 //routes params to get SPECIFIC PRODUCT FROM THE OBJECT
 
-app.get("/products/:productID" , (req, res)=>{
+app.get("/api/products/:productID", (req, res) => {
+  const IDparam = req.params.productID;
+  console.log(IDparam);
+  if (IDparam > products.length || IDparam < 0) {
+    res.send("OVERFLOW");
+  } else {
+    res.send(products[IDparam]);
+  }
+});
 
-const IDparam = req.params.productID
-  console.log(IDparam)
-if (IDparam > products.length || IDparam < 0){
+//send the json file of the products API
+app.get("/api/products", (req, res) => {
+  // res.json(products);
+  
+  res.json(products)
 
-  res.send("OVERFLOW")
 
-}else{
-res.json(products[IDparam])}
-})
-
+});
